856a709b488e75217e4bdd10181ebcdb9882d403
unknown <csernt@yandex.ru>
2002-01-01 05:26:46 +0300

    TextArea appendText fix

--- src/temperate/text/CTextArea.hx
+++ src/temperate/text/CTextArea.hx
@@ -401,7 +401,7 @@ class CTextArea extends ACScrollPane
 	
 	public function appendText(text:String)
 	{
-		_text += _text != null ? _text + text : text;
+		_text = _text != null ? _text + text : text;
 		if (_html)
 		{
 			_tf.htmlText = _text != null ? _text : "";
