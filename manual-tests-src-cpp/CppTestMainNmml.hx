1e5e7c4b355897f1994920efd7c4f056754a3625
unknown <expl@.(none)>
2012-02-26 17:28:11 +0300

    DebugMonitor now compile on nme

--- manual-tests-src-cpp/CppTestMainNmml.hx
+++ manual-tests-src-cpp/CppTestMainNmml.hx
@@ -2,6 +2,7 @@ package ;
 import flash.display.StageAlign;
 import flash.display.StageScaleMode;
 import flash.Lib;
+import minimal.TestMButton;
 
 class CppTestMainNmml
 {
@@ -17,7 +18,38 @@ class CppTestMainNmml
 		*/
 		var test = new NmeTestText();
 		var test = new NmeTestCurrent();
-		//var text = new TestMButton();
+		
+		var test = new TestMButton();
+		var test = new TestDebugMonitor();
+		/*var test = new TestMButton();
+		var test = new TestFrameEvent();
+		var test = new TestContainer();
+		var test = new TestContainerSpace();
+		var test = new TestRecursiveContainer();
+		var test = new TestMCheckBox();
+		var test = new TestTooltipsOld();
+		var test = new TestMTooltips();
+		var test = new TestNumericStepper();
+		var test = new TestCursorManager();
+		var test = new TestMCursorManager();
+		var test = new TestMCharts();
+		var test = new TestText();
+		var test = new TestMTween();
+		var test = new TestTooltips();
+		var test = new TestScrollBar();
+		var test = new TestTextArea();
+		var test = new TestSlider();
+		var test = new TestSimpleButtonWrapper();
+		var test = new TestSignalPerformance();
+		var test = new TestScrollPane();
+		var test = new TestKey();
+		var test = new TestRasterImageButton();
+		
+		var test = new TestWindows();
+		var test = new TestValidationBug();
+		var test = new TestWindowApplication();
+		*/
+		
 		Lib.current.addChild(test);
 		test.init();
 	}
